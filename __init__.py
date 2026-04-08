1b8c8eb0210614208676a44a04136b67a588da1d
FerraXIDE <gabrielsanchezferra@gmail.com>
2026-04-08 01:21:58 -0600

    __init__.py

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ from .core import Text4QCompiler, QuantumCommand
 from .gates import GateFactory
 from .validator import CommandValidator
 
-__version__ = "1.0.3"
+__version__ = "1.0.1"
 __all__ = ['Text4QCompiler', 'QuantumCommand', 'GateFactory', 'CommandValidator']
