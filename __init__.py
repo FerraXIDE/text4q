b2a94753bb0ee6a382d345dd98117a55c4e76d7e
FerraXIDE <gabrielsanchezferra@gmail.com>
2026-04-08 01:38:25 -0600

    Downgrade version from 1.1.0 to 1.0.2

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ from .core import Text4QCompiler, QuantumCommand
 from .gates import GateFactory
 from .validator import CommandValidator
 
-__version__ = "1.1.0"
+__version__ = "1.0.2"
 __all__ = ['Text4QCompiler', 'QuantumCommand', 'GateFactory', 'CommandValidator']
