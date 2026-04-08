8780f30eff3fd6fec4abde4bbcf9d732d207b094
FerraXIDE <gabrielsanchezferra@gmail.com>
2026-04-08 03:15:55 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,5 +6,5 @@ from .core import Text4QCompiler
 from .validator import validate_commands
 from .gates import GateType
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __all__ = ['Text4QCompiler', 'validate_commands', 'GateType']
