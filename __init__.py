2b1ee104b5e9020da02b0ff8ec6098cebf9b5d50
FerraXIDE <gabrielsanchezferra@gmail.com>
2026-04-08 00:09:55 -0600

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+text4q - Natural command language for quantum computing
+
+from .core import Text4QCompiler, QuantumCommand
+from .gates import GateFactory
+from .validator import CommandValidator
+
+__version__ = "1.0.0"
+__all__ = ['Text4QCompiler', 'QuantumCommand', 'GateFactory', 'CommandValidator']
