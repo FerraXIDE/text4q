45578bcdbac315d610d0eb0eaf9394d1d6540ca8
FerraXIDE <gabrielsanchezferra@gmail.com>
2026-04-08 04:46:20 -0600

    Rename validate_commands to CommandValidator

--- text4q/__init__.py
+++ text4q/__init__.py
@@ -3,8 +3,8 @@ text4q
 """
 
 from .core import Text4QCompiler
-from .validator import validate_commands
+from .validator import CommandValidator
 from .gates import GateType
 
 __version__ = "1.0.5"
-__all__ = ['Text4QCompiler', 'validate_commands', 'GateType']
+__all__ = ['Text4QCompiler', 'CommandValidator', 'GateType']
