d3533acf99a5e51d00788366635b318c6081a773
FerraXIDE <gabrielsanchezferra@gmail.com>
2026-04-08 01:38:49 -0600

    Downgrade version from 1.1.0 to 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="text4q",
-    version="1.1.0",
+    version="1.0.2",
     author="Gabriel De Jesús Sánchez Ferra",
     author_email="gabrielsanchezferra@gmail.com",
     description="Natural command language for quantum computing",
